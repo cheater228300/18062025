1d80e899ab4e2f17cf5b32a39d23f990b0bbeb0a
basedgodlife <j2i2d@mail.ru>
2025-06-17 22:55:37 +0700

    Add files via upload

--- tasks/051-100/058/main.c
+++ tasks/051-100/058/main.c
@@ -4,9 +4,30 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 058\n");
+int main()
+{
+    int number;
+
+    printf("Введите число: ");
+    scanf("%d", &number);
+
+    if (number < 0 && number % 3 == 0)
+    {
+        printf("Условие 1: отрицательное и делится на 3.\n");
+    }
+    else if (number >= 10 && number <= 20 && number % 2 == 0)
+    {
+        printf("Условие 2: 10-20 и чётное.\n");
+    }
+    else if (number == 100)
+    {
+        printf("Условие 3: равно 100.\n");
+    }
+    else
+    {
+        printf("Не подходит ни под одно условие.\n");
+    }
+
     return 0;
 }
