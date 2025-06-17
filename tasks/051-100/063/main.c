c49f011ab100584f5dbba726874a823005dbaa86
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:11:52 +0300

    Update main.c

--- tasks/051-100/063/main.c
+++ tasks/051-100/063/main.c
@@ -4,9 +4,10 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    printf("Solution for task 063\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
     return 0;
 }
