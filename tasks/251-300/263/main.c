19510575812cbcfdd636d894fceeba7c78d92357
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:50:13 +0700

    Add files via upload

--- tasks/251-300/263/main.c
+++ tasks/251-300/263/main.c
@@ -4,9 +4,35 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 263\n");
+int main()
+{
+    FILE *output = fopen("combined.txt", "w");
+    if (!output)
+    {
+        perror("Ошибка создания файла");
+        return 1;
+    }
+
+    const char *files[] = {"file1.txt", "file2.txt"};
+    for (int i = 0; i < 2; i++)
+    {
+        FILE *input = fopen(files[i], "r");
+        if (!input)
+        {
+            perror("Ошибка открытия файла");
+            continue;
+        }
+
+        char ch;
+        while ((ch = fgetc(input)) != EOF)
+        {
+            fputc(ch, output);
+        }
+        fclose(input);
+    }
+
+    fclose(output);
+    printf("Файлы объединены в 'combined.txt'\n");
     return 0;
 }
