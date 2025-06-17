eb1f255bd735254865e1ee58d8ae88093cc7b6be
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:08:27 +0300

    Обнова

--- tasks/051-100/061/main.c
+++ tasks/051-100/061/main.c
@@ -4,9 +4,27 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>  // для pow, если используешь, но тут не нужно
+
+int is_inside_circle(double x, double y, double cx, double cy, double r) {
+    double dx = x - cx;
+    double dy = y - cy;
+    return (dx * dx + dy * dy) < (r * r);
+}
 
 int main() {
-    printf("Solution for task 061\n");
+    double x, y;
+    double cx = 0.0, cy = 0.0, r = 5.0;
+
+    printf("Введите координаты точки (x y): ");
+    scanf("%lf %lf", &x, &y);
+
+    if (is_inside_circle(x, y, cx, cy, r)) {
+        printf("Точка внутри окружности\n");
+    } else {
+        printf("Точка вне окружности или на границе\n");
+    }
+
     return 0;
 }
+
