43b1498385f01d2c2d8c68d11a2bebcd96610fa7
basedgodlife <j2i2d@mail.ru>
2025-06-17 22:50:52 +0700

    Add files via upload

--- tasks/051-100/051/main.c
+++ tasks/051-100/051/main.c
@@ -4,9 +4,29 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 051\n");
+void toUpperCase(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - ('a' - 'A');
+        }
+        i++;
+    }
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Введите строку: ");
+    fgets(str, sizeof(str), stdin);
+
+    toUpperCase(str);
+    printf("Результат: %s", str);
+
     return 0;
 }
