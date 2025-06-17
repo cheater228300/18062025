88caef185b98d201cf999798945dcad9e783ece0
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:26:32 +0300

    Update main.c

--- tasks/101-150/134/main.c
+++ tasks/101-150/134/main.c
@@ -4,9 +4,43 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
 int main() {
-    printf("Solution for task 134\n");
+    const char *filename = "input.txt"; //Нужно создать текстовый файл input.txt с каким-то текстом
+    FILE *file = fopen(filename, "r");
+
+    if (!file) {
+        perror("Ошибка при открытии файла");
+        return 1;
+    }
+
+    int ch;
+    int in_word = 0;
+    int word_count = 0;
+    int line_count = 0;
+    int char_count = 0;
+
+    while ((ch = fgetc(file)) != EOF) {
+        char_count++;
+
+        if (ch == '\n')
+            line_count++;
+
+        if (isspace(ch)) {
+            in_word = 0;
+        } else if (!in_word) {
+            in_word = 1;
+            word_count++;
+        }
+    }
+
+    fclose(file);
+
+    printf("Файл: %s\n", filename);
+    printf("Символов: %d\n", char_count);
+    printf("Слов:     %d\n", word_count);
+    printf("Строк:    %d\n", line_count);
+
     return 0;
 }
