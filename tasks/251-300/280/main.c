a9bcba0ed5a71f82aa9f84186e712fda4d2a7872
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 22:09:12 +0300

    Update main.c

--- tasks/251-300/280/main.c
+++ tasks/251-300/280/main.c
@@ -4,9 +4,25 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+// Очищает все биты, кроме K старших (для 32-битных чисел)
+unsigned int keep_top_k_bits(unsigned int num, int k) {
+    if (k >= 32) return num;       // все биты остаются
+    if (k <= 0) return 0;          // всё очищается
+
+    unsigned int mask = ~((1U << (32 - k)) - 1); // маска с K старшими битами = 1
+    return num & mask;
+}
 
 int main() {
-    printf("Solution for task 280\n");
+    unsigned int number = 0b11101100101010001111000000001111;
+    int k = 8;
+
+    unsigned int result = keep_top_k_bits(number, k);
+
+    printf("До : 0x%08X\n", number);
+    printf("После оставления %d старших битов: 0x%08X\n", k, result);
+
     return 0;
 }
+
