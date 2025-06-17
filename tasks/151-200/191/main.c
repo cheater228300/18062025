8f69f44ff8689de87aaf1e3e3c390132868061c5
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:32:34 +0700

    Add files via upload

--- tasks/151-200/191/main.c
+++ tasks/151-200/191/main.c
@@ -2,11 +2,14 @@
 Условие задачи 191:
 191. Напишите макрос MAX(a, b), возвращающий большее из двух чисел.
 */
-
+//
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 191\n");
+#define MAX(a, b) ((a) > (b) ? (a) : (b)) // Тернарный оператор выбирает максимум
+
+int main()
+{
+    int x = 5, y = 10;
+    printf("Max: %d\n", MAX(x, y)); // Выведет 10
     return 0;
 }
