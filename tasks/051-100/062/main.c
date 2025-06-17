2b1e8b154f5d793fa52de84cfe8d51017e0fb31f
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:11:26 +0300

    Update main.c

--- tasks/051-100/062/main.c
+++ tasks/051-100/062/main.c
@@ -4,9 +4,22 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+int is_strictly_increasing(int a, int b, int c) {
+    return a < b && b < c;
+}
 
 int main() {
-    printf("Solution for task 062\n");
+    int a, b, c;
+
+    printf("Введите три числа через пробел: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (is_strictly_increasing(a, b, c)) {
+        printf("Числа возрастают строго: %d < %d < %d\n", a, b, c);
+    } else {
+        printf("Числа НЕ возрастают строго\n");
+    }
+
     return 0;
 }
