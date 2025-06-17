dfd7cce26ec51fe3390d56ed9e49b94946f141cb
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:30:38 +0300

    Update main.c

--- tasks/101-150/139/main.c
+++ tasks/101-150/139/main.c
@@ -4,9 +4,26 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+int max_in_array(int *arr, int n) {
+    if (n <= 0) return -2147483648;
+    int max = *arr;
+    int *ptr = arr + 1;
+    for (int i = 1; i < n; i++, ptr++) {
+        if (*ptr > max) {
+            max = *ptr;
+        }
+    }
+    return max;
+}
 
 int main() {
-    printf("Solution for task 139\n");
+    int data[] = {10, 5, 20, 3, 15};
+    int n = sizeof(data) / sizeof(data[0]);
+
+    int maximum = max_in_array(data, n);
+    printf("Максимальное значение в массиве: %d\n", maximum);
+
     return 0;
 }
+
