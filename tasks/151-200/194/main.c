873c556cc451b368fe7862dd913ca928ad414b28
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:33:22 +0700

    Add files via upload

--- tasks/151-200/194/main.c
+++ tasks/151-200/194/main.c
@@ -4,9 +4,18 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 194\n");
+#define DEBUG 1 // Флаг отладки (1 = включено)
+
+#if DEBUG
+#define DEBUG_PRINT(...) printf("DEBUG: " __VA_ARGS__) // Вывод, если DEBUG=1
+#else
+#define DEBUG_PRINT(...) // Пустой макрос, если DEBUG=0
+#endif
+
+int main()
+{
+    int x = 42;
+    DEBUG_PRINT("x = %d\n", x); // Выведет "DEBUG: x = 42"
     return 0;
 }
