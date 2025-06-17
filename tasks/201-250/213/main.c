b80e501b09118abfc4e110a66e0d2982c739cc52
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:55:19 +0300

    Update main.c

--- tasks/201-250/213/main.c
+++ tasks/201-250/213/main.c
@@ -5,7 +5,19 @@
 #include <stdlib.h>
 
 int main() {
-    int* arr=malloc(100*sizeof(int));
+    int* arr = malloc(100 * sizeof(int));
+//  if (!arr) {
+//      perror("malloc failed");
+//      return 1;
+//  }
 
+    // Комментарии убираем только после того, как в valgrind выдало ошибку
+
+//  free(arr); // Освобождаем память
     return 0;
 }
+
+
+//gcc -g main.c -o prog
+//valgrind --leak-check=full ./prog
+//выключаем терминал и убираем комментарии, потом по новой
