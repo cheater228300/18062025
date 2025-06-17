345499e3bd17c55c589ca7626225767581d3e54a
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 22:00:30 +0300

    Update main.c

--- tasks/251-300/271/main.c
+++ tasks/251-300/271/main.c
@@ -4,9 +4,23 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+
+// Проверяет, установлен ли бит под номером bit_index (нумерация с 0)
+bool is_bit_set(unsigned int num, int bit_index) {
+    return (num & (1 << bit_index)) != 0;
+}
 
 int main() {
-    printf("Solution for task 271\n");
+    unsigned int number = 42; // 0b101010
+    int bit_to_check = 3;
+
+    if (is_bit_set(number, bit_to_check)) {
+        printf("Бит %d установлен в числе %u\n", bit_to_check, number);
+    } else {
+        printf("Бит %d НЕ установлен в числе %u\n", bit_to_check, number);
+    }
+
     return 0;
 }
+
