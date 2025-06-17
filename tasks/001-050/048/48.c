b648c6498846a12457d595af4d7968b71b13f4c1
Jeroyg <qwerhail73@gmail.com>
2025-06-18 00:57:15 +0700

    Add files via upload

---
+++ tasks/001-050/048/48.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int my_strlen(const char *str) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    
+    int len = my_strlen(str);
+    if (str[len - 1] == '\n') {
+        len--;
+    }
+    
+    printf("Length of string: %d\n", len);
+    return 0;
+}
