b7d19802774487fcfe01186f2bc6dd16f7a0b868
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:51:26 +0700

    Add files via upload

--- tasks/251-300/268/main.c
+++ tasks/251-300/268/main.c
@@ -4,9 +4,32 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 268\n");
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Использование: %s <исходный_файл> <целевой_файл>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dest = fopen(argv[2], "wb");
+
+    if (!src || !dest)
+    {
+        perror("Ошибка открытия файла");
+        return 1;
+    }
+
+    char byte;
+    while (fread(&byte, 1, 1, src))
+    {
+        fwrite(&byte, 1, 1, dest);
+    }
+
+    fclose(src);
+    fclose(dest);
+    printf("Файл скопирован!\n");
     return 0;
 }
