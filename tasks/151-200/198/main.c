3bf08ee3aec7538aa7e22ed6eba69ad49d909d77
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:34:29 +0700

    Add files via upload

--- tasks/151-200/198/main.c
+++ tasks/151-200/198/main.c
@@ -4,9 +4,16 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 198\n");
+#define LOG_LEVEL 2 // Сообщения с level <= 2 выводятся
+
+#define LOG(level, ...)     \
+    if (level <= LOG_LEVEL) \
+    printf("LOG[%d]: " __VA_ARGS__)
+
+int main()
+{
+    LOG(1, "Critical error\n"); // Выведется (1 <= 2)
+    LOG(3, "Debug info\n");     // Не выведется (3 > 2)
     return 0;
 }
