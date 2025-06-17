6c7dcab89b0f731e3d5d086abbbfc3b770569fce
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:32:51 +0700

    Add files via upload

--- tasks/151-200/192/main.c
+++ tasks/151-200/192/main.c
@@ -2,11 +2,20 @@
 Условие задачи 192:
 192. Реализуйте безопасный макрос SWAP(a, b, type) для обмена значений.
 */
-
+// gcc main.c -o swap_example && ./swap_example
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 192\n");
+#define SWAP(a, b, type) \
+    {                    \
+        type temp = a;   \
+        a = b;           \
+        b = temp;        \
+    } // Обмен через временную переменную
+
+int main()
+{
+    int x = 5, y = 10;
+    SWAP(x, y, int); // После обмена x=10, y=5
+    printf("x=%d, y=%d\n", x, y);
     return 0;
 }
