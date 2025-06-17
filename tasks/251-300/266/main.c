d6387b3b4db020a80ab0ff2b63f715dae3cfba57
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:50:59 +0700

    Add files via upload

--- tasks/251-300/266/main.c
+++ tasks/251-300/266/main.c
@@ -4,9 +4,22 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 266\n");
+void append_to_file(const char *filename, const char *text)
+{
+    FILE *file = fopen(filename, "a"); // Режим 'a' = append
+    if (!file)
+    {
+        perror("Ошибка открытия файла");
+        return;
+    }
+    fprintf(file, "%s\n", text);
+    fclose(file);
+}
+
+int main()
+{
+    append_to_file("log.txt", "Новая запись");
+    printf("Запись добавлена!\n");
     return 0;
 }
