c7ec60b7da9fd7f1cfc2077196746743c65f3d07
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 22:07:54 +0300

    Update main.c

--- tasks/251-300/279/main.c
+++ tasks/251-300/279/main.c
@@ -4,9 +4,27 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+// Устанавливает N младших битов в 1
+unsigned int set_low_bits(unsigned int num, int n) {
+    unsigned int mask = (1U << n) - 1; // создаёт маску с n единицами
+    return num | mask;
+}
 
 int main() {
-    printf("Solution for task 279\n");
+    unsigned int number = 0b10010000; // 144
+    int n = 4;
+
+    unsigned int result = set_low_bits(number, n);
+
+    printf("До : 0b");
+    for (int i = 7; i >= 0; i--) printf("%d", (number >> i) & 1);
+    printf("\n");
+
+    printf("После установки %d младших битов: 0b", n);
+    for (int i = 7; i >= 0; i--) printf("%d", (result >> i) & 1);
+    printf("\n");
+
     return 0;
 }
+
