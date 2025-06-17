c07c8789228bcb9e58cb8c4cfb2c606104041a1b
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:46:13 +0300

    Update main.c

--- tasks/201-250/210/main.c
+++ tasks/201-250/210/main.c
@@ -4,9 +4,26 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    printf("Solution for task 210\n");
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+    printf("Сумма %d + %d = %d\n", a, b, sum);
+
+    for (int i = 0; i < 3; i++) {
+        printf("Итерация %d\n", i);
+    }
+
     return 0;
 }
+
+//gcc -g main.c -o test
+//gdb ./test
+//break main
+//run
+//print b
+//next
+//quit
+
+
