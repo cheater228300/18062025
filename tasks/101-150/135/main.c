961793a83f7a7ac3c0160b1b5d349abd4cd98421
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:27:12 +0300

    Update main.c

--- tasks/101-150/135/main.c
+++ tasks/101-150/135/main.c
@@ -4,9 +4,23 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+// Функция вычисления факториала n (для n >= 0)
+unsigned long long factorial(int n) {
+    if (n < 0) {
+        return 0; // Факториал для отрицательных чисел не определён
+    }
+
+    unsigned long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
 
 int main() {
-    printf("Solution for task 135\n");
+    int num = 5;
+    printf("Факториал %d = %llu\n", num, factorial(num));
     return 0;
 }
+
