20b015e397e96ae6a87e52022a4221984d237c78
basedgodlife <j2i2d@mail.ru>
2025-06-17 22:52:09 +0700

    Add files via upload

--- tasks/051-100/053/main.c
+++ tasks/051-100/053/main.c
@@ -4,9 +4,43 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() {
-    printf("Solution for task 053\n");
+#define MAX_WORDS 100
+#define MAX_WORD_LEN 50
+
+void findCapitalizedWords(char *str)
+{
+    char *words[MAX_WORDS];
+    int wordCount = 0;
+
+    char *token = strtok(str, " \t\n");
+    while (token != NULL && wordCount < MAX_WORDS)
+    {
+        words[wordCount] = token;
+        wordCount++;
+        token = strtok(NULL, " \t\n");
+    }
+
+    printf("Слова с заглавной буквы:\n");
+    for (int i = 0; i < wordCount; i++)
+    {
+        if (isupper(words[i][0]))
+        {
+            printf("%s\n", words[i]);
+        }
+    }
+}
+
+int main()
+{
+    char input[256];
+
+    printf("Введите строку: ");
+    fgets(input, sizeof(input), stdin);
+
+    findCapitalizedWords(input);
+
     return 0;
 }
