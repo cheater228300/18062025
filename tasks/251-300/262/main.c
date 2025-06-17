c1f109e84142ffe08c4d6fe3b259d97391a5d678
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:49:46 +0700

    Add files via upload

--- tasks/251-300/262/main.c
+++ tasks/251-300/262/main.c
@@ -6,7 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Solution for task 262\n");
+int main()
+{
+    const char *filename = "example.txt";
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        perror("Ошибка открытия файла");
+        printf("Файл '%s' не существует!\n", filename);
+        return 1;
+    }
+
+    printf("Файл '%s' успешно открыт!\n", filename);
+    fclose(file);
     return 0;
 }
