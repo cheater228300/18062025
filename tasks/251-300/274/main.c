e04115a31de80840f90a17d5cbe754c1585cdf72
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 22:02:31 +0300

    Update main.c

--- tasks/251-300/274/main.c
+++ tasks/251-300/274/main.c
@@ -4,9 +4,21 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+// Подсчёт количества установленных битов (единиц) в числе
+int count_set_bits(unsigned int num) {
+    int count = 0;
+    while (num) {
+        count += num & 1;  // Прибавляем 1, если младший бит равен 1
+        num >>= 1;         // Сдвигаем число вправо на 1 бит
+    }
+    return count;
+}
 
 int main() {
-    printf("Solution for task 274\n");
+    unsigned int number = 29; // 0b11101
+    int result = count_set_bits(number);
+    printf("В числе %u установлено %d бит(а)\n", number, result);
     return 0;
 }
+
