83a2ac81fe3936ef3bc1dd19fe56100708918e46
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 22:16:48 +0300

    Update main.c

--- tasks/301-350/331/main.c
+++ tasks/301-350/331/main.c
@@ -4,9 +4,26 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+
+bool has_duplicates(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j])
+                return true;
+        }
+    }
+    return false;
+}
 
 int main() {
-    printf("Solution for task 331\n");
+    int a[] = {3, 1, 4, 2, 5, 3};
+    int size = sizeof(a) / sizeof(a[0]);
+
+    if (has_duplicates(a, size))
+        printf("Массив содержит дубликаты.\n");
+    else
+        printf("Массив не содержит дубликатов.\n");
+
     return 0;
 }
