eb0b58840a631d601d8f203e9029f36ed414872c
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:27:43 +0300

    Update main.c

--- tasks/101-150/136/main.c
+++ tasks/101-150/136/main.c
@@ -4,9 +4,18 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+int sum_array(int *arr, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
 
 int main() {
-    printf("Solution for task 136\n");
+    int data[] = {1, 2, 3, 4, 5};
+    int size = sizeof(data) / sizeof(data[0]);
+    printf("Сумма элементов: %d\n", sum_array(data, size));
     return 0;
 }
