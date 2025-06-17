5e5923c9ceb19b408aa150e9fb247fdb26ce3384
IvAce <98728069+IvAce@users.noreply.github.com>
2025-06-17 21:19:32 +0300

    Update main.c

--- tasks/101-150/132/main.c
+++ tasks/101-150/132/main.c
@@ -2,11 +2,57 @@
 Условие задачи 132:
 132. Напишите программу, которая считывает файл и записывает все слова в другой файл в обратном порядке.
 */
+//гробик :)
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_WORDS 10000
+#define MAX_WORD_LEN 100
 
 int main() {
-    printf("Solution for task 132\n");
+    FILE *in = fopen("input.txt", "r");
+    FILE *out = fopen("output.txt", "w");
+
+    if (!in || !out) {
+        printf("Ошибка при открытии файла.\n");
+        return 1;
+    }
+
+    char *words[MAX_WORDS];
+    int count = 0;
+    char word[MAX_WORD_LEN];
+
+    // Считываем все слова
+    while (fscanf(in, "%99s", word) == 1) {
+        words[count] = malloc(strlen(word) + 1);
+        if (!words[count]) {
+            printf("Ошибка выделения памяти.\n");
+            return 1;
+        }
+        strcpy(words[count], word);
+        count++;
+
+        if (count >= MAX_WORDS) {
+            printf("Превышено максимальное число слов.\n");
+            break;
+        }
+    }
+
+    // Записываем слова в обратном порядке
+    for (int i = count - 1; i >= 0; i--) {
+        fprintf(out, "%s", words[i]);
+        if (i > 0) fprintf(out, " ");
+        free(words[i]);  // освобождение памяти
+    }
+
+    fprintf(out, "\n");
+
+    fclose(in);
+    fclose(out);
+
+    printf("Слова успешно записаны в обратном порядке в output.txt\n");
     return 0;
 }
