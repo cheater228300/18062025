c254dd0c7d248a0504b29e5d00e1b6f39e67f765
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:51:54 +0700

    Add files via upload

--- tasks/251-300/270/main.c
+++ tasks/251-300/270/main.c
@@ -4,9 +4,16 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 270\n");
+int clear_bit(int num, int pos)
+{
+    return num & ~(1 << pos); // Очищает бит в позиции pos
+}
+
+int main()
+{
+    int num = 7;                                    // 0111 в двоичном виде
+    int pos = 1;                                    // Очистим 1-й бит
+    printf("Результат: %d\n", clear_bit(num, pos)); // 5 (0101)
     return 0;
 }
