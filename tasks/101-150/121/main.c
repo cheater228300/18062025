ca19bd62657eea25d34addc03d663b8235f3429a
basedgodlife <j2i2d@mail.ru>
2025-06-17 23:02:19 +0700

    Add files via upload

--- tasks/101-150/121/main.c
+++ tasks/101-150/121/main.c
@@ -4,9 +4,22 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    printf("Solution for task 121\n");
+int main()
+{
+    FILE *file = fopen("example.txt", "r");
+    if (file == NULL)
+    {
+        printf("Ошибка открытия файла!\n");
+        return 1;
+    }
+
+    char ch;
+    while ((ch = fgetc(file)) != EOF)
+    {
+        putchar(ch);
+    }
+
+    fclose(file);
     return 0;
 }
